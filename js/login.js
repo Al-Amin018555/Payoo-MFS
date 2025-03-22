5e33d6896aa4069a4e90cf9466831951623c70fa
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-03-22 12:18:46 +0600

    added form validation & redirect to home page

--- js/login.js
+++ js/login.js
@@ -1,11 +1,20 @@
 // step-1: set event handler
-document.getElementById('btn-login').addEventListener('click',function(event){
+document.getElementById('btn-login').addEventListener('click', function (event) {
 
     // step-2: prevent default behaviour (prevent reloading browser)
     event.preventDefault();
 
-    // step-3: get the phone number
+    // step-3: get the phone number and pin number
     const mobileNumber = document.getElementById('mobile-number').value;
-    console.log(mobileNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+
+    // step-4: validation (this is not the perfect way to do so. here we are doing it just temporarily)
+    if (mobileNumber === '5' && pinNumber === '1234'){
+        console.log('Your are logged in');
+        window.location.href = 'home.html';
+    }
+    else{
+        alert('Wrong number or pin.');
+    }
 
 });
