f17dc565585d834c2ac20caf9f692888207af7a7
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-03-22 13:48:40 +0600

    made add money update live in available balance

--- js/home.js
+++ js/home.js
@@ -3,18 +3,39 @@
   s-1: add event handler
   prevent page reload after form submit
   s-2: get money to be added to the account balance
+  get the pin number
+  s-3: verify the pin number
  */
 
 // step-1: add an event handler to the add money button inside the form
 document.getElementById('btn-add-money').addEventListener('click',function(aw){
-    //revent page reload after form submit
+    //prevent page reload after form submit
     aw.preventDefault();
 
     // step-2: et money to be added to the account
     const moneyAmountInput = document.getElementById('input-amount').value;
+
     //get pin number provided
     const inputPinNumber = document.getElementById('input-pin').value;
-    console.log(moneyAmountInput,inputPinNumber);
+
+    // step-3: verify pin number
+    //wrong way to validate pin number
+    if(inputPinNumber === '1234'){
+       //step-4: get the current balance
+        const currentBalance = document.getElementById('current-balance').innerText;
+
+        // step-5:add currentBalance with moneyAmountInput
+        const currentBalanceNumber = parseFloat(currentBalance);
+        const moneyAmountNumber = parseFloat(moneyAmountInput);
+        const totalBalance = currentBalanceNumber + moneyAmountNumber;
+       
+        // step-6: update the balance in the UI/DOM
+        document.getElementById('current-balance').innerText = totalBalance;
+        
+    }
+    else{
+        console.log('wrong password! please try again');
+    }
 });
 
 
