cfe910970e770616e7f602d6f412090e96805bda
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-03-22 15:36:17 +0600

    added some comments on features cashout and add money event listeners

--- js/features.js
+++ js/features.js
@@ -1,10 +1,14 @@
+//show cash out form
 document.getElementById('show-cash-out').addEventListener('click',function(){
     document.getElementById('cash-out-form').classList.remove('hidden');
+    //hide the add money form
     document.getElementById('add-money-form').classList.add('hidden');
 });
 
+// show add money form
 document.getElementById('show-add-money').addEventListener('click',function(){
     document.getElementById('add-money-form').classList.remove('hidden');
+    //hide the cash out form
     document.getElementById('cash-out-form').classList.add('hidden');
 
 });
