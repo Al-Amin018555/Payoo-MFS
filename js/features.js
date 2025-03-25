49d4cacf083e9162e378299c645bd162d202566b
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-03-25 14:15:13 +0600

    made toogling between add money and cash out feature

---
+++ js/features.js
@@ -0,0 +1,13 @@
+document.getElementById('show-add-money')
+    .addEventListener('click', function () {
+        // console.log('clicked for add money features');
+        showSectionById('add-money-form')
+    });
+
+
+document.getElementById('show-cash-out')
+    .addEventListener('click', function () {
+
+        // console.log('clicked for having cashout feature');
+        showSectionById('cash-out-form')
+    });
